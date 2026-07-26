4c84ef8305c3cd44c321b9182558675ec13322ee
Indri Kartika <indri.kartikasari007@gmail.com>
2026-07-26 15:40:14 +0700

    chore: remove temporary hack and audit scripts

--- list_gemini.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-from google import genai
-load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-for m in client.models.list(config={"page_size": 50}):
-    print(m.name)
